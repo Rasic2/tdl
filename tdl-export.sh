705bf35ec5059c89665594a3ea2644b85afa6778
Rasic2 <1051987201@qq.com>
2024-05-01 19:24:39 +0800

    add check in tdl-export.sh

--- tdl-export.sh
+++ tdl-export.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
 
-cat tdl-chatlist
+IFS_old=$IFS
+IFS=$'\n'
+go run main.go chat ls > tdl-chatlist
+for chat in `cat tdl-chatlist | sed -n "2,$ p"`
+do
+    id=`echo $chat | awk '{print $1}'`
+    modify_time=`ls -l -D "%Y%m%d" downloads | grep "$id" | awk '{print $6}' 2>/dev/null` 
+    if [ -z $modify_time ];then
+        echo $chat
+    else
+        now=`date "+%Y%m%d"`
+        if [ $now != $modify_time ];then
+            echo $chat
+        fi
+    fi
+done
+IFS=$IFS_old
+
 echo -e "\033[1;31m --> tdl chat export \033[0m"
 #go run main.go chat export -c $1 -f "Views>200 && Media.Name not endsWith '.jpg'"
 go run main.go chat export -c $1
