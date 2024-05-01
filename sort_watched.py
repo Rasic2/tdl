229c5ac35e81ccfe106dda6556739c1164ebe479
Rasic2 <1051987201@qq.com>
2024-05-01 20:27:12 +0800

    add sort_watched.py

---
+++ sort_watched.py
@@ -0,0 +1,8 @@
+with open("watched", "r") as f:
+    content = [line.split(",")[0] for line in f.readlines()]
+
+sorted_list = []
+for item in content:
+    if item not in sorted_list:
+        sorted_list.append(item)
+print(sorted_list)
