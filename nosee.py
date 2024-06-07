23d302a0508b60f086e1861ca92e8bc581c9f51c
Rasic2 <1051987201@qq.com>
2024-06-07 22:36:33 +0800

    update nosee.py

--- nosee.py
+++ nosee.py
@@ -13,7 +13,8 @@ sorted_total = sorted(total, key=lambda x: x['emoji_count'], reverse=True)
 
 count = 0
 for item in sorted_total:
-    if not Path(f'downloads/{item["item_id"]}/{item["item_id"]}_{item["id"]}_{item["file"].replace(" ","_")}').exists():
+    file_exist = Path(f'downloads/{item["item_id"]}').glob(f'{item["item_id"]}_{item["id"]}_*')
+    if not (list(file_exist)):
         count += 1
         print(item["item_id"], item["file"], item["emoji_count"])
     if count >= 10:
